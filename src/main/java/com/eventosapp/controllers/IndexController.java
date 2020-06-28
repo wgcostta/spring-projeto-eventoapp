a79969bb9f290587d9d3b82443eb7cd2dedf8418
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-27 22:40:37 -0300

    Renderizando a API migrando

--- src/main/java/com/eventosapp/controllers/IndexController.java
+++ src/main/java/com/eventosapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.eventoapp.eventoapp.Controller;
+package com.eventosapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String index(){
-        return "index";
-    }
-
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
 }
